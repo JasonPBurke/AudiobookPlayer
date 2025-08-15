2da79d167baee33ecf5807927d45e1df3d9f4f75
Jason Burke <jpburke77@gmail.com>
2025-08-15 15:25:15 -0600

    Chore: Book List Item

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import "./global.css"
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import BookListItem from "./src/components/BookListItem";
+
+import books from "./src/dummyBooks";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!!
-      </Text>
+    <View className="bg-slate-700 flex-1 justify-center p-4 ">
+      <BookListItem book={books[0]} />
+      <BookListItem book={books[1]} />
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/BookListItem.tsx
@@ -0,0 +1,30 @@
+import { Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  audio_url: string;
+  thumbnail_url?: string;
+};
+
+type BookListItemProps = {
+  book: Book;
+};
+
+export default function BookListItem({ book }: BookListItemProps) {
+  return (
+    <View className="flex-row gap-4 items-center">
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="w-20 aspect-square rounded-sm"
+      />
+      <View className="gap-2 flex-1">
+        <Text className="text-xl font-bold text-gray-100">{book.title}</Text>
+        <Text className="text-gray-400">{book.author}</Text>
+      </View>
+      <AntDesign className="" name="playcircleo" size={24} color="gainsboro" />
+    </View>
+  );
+}
